34ad6bce98bfa8a30087511d591414eddebe7fb2
Kirill Makeev <makeevbang@gmail.com>
2024-09-17 10:23:28 +0300

    s9 json without tests

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,14 +1,15 @@
 from gendiff.path import read_files
 from gendiff.build_diff import build_diff
 from gendiff.formater.stylish import get_stylish
-# from gendiff.formater.plane import get_stylish
+from gendiff.formater.plane import get_plain
+from gendiff.formater.json import get_json
 
 
 def generate_diff(first_file, second_file, format='stylish'):
     file1, file2 = read_files(first_file, second_file)
     diff = build_diff(file1, file2)
-    formated = get_stylish(diff)
+    formated = get_json(diff)
     return print(formated)
 
 
-generate_diff('gendiff/files/file1_tree.yml', 'gendiff/files/file2_tree.yml')
+generate_diff('gendiff/files/file1_tree.json', 'gendiff/files/file2_tree.json')
---
+++ gendiff/formater/plane.py
@@ -0,0 +1,30 @@
+def get_plain(diff, path=''):
+    lines = []
+    for key, val in diff.items():
+        property_path = f"{path}{key}"
+        if val['type'] == 'added':
+            lines.append(f"Property '{property_path}' "
+                         f"was added with value: "
+                         f"{str_value(val['value'])}")
+        if val['type'] == 'removed':
+            lines.append(f"Property '{property_path}' was removed")
+        if val['type'] == 'nested':
+            nested_value = get_plain(val['value'], f"{property_path}.")
+            lines.append(f"{nested_value}")
+        if val['type'] == 'changed':
+            lines.append(f"Property '{property_path}' was updated. "
+                         f"From {str_value(val['old_value'])} to "
+                         f"{str_value(val['new_value'])}")
+    return '\n'.join(lines)
+
+
+def str_value(value):
+    if isinstance(value, dict):
+        return "[complex value]"
+    if isinstance(value, bool):
+        return str(value).lower()
+    if isinstance(value, int):
+        return value
+    if value is None:
+        return "null"
+    return f"'{value}'"
